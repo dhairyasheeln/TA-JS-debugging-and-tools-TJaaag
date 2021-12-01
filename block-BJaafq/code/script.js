e01010666607185c3c8007c461eb3007964bae91
dhairyasheel <dhairyasheeln@gmail.com>
2021-12-01 15:10:16 +0530

    Test framework and assertion

--- block-BJaafq/code/script.js
+++ block-BJaafq/code/script.js
@@ -7,20 +7,51 @@ function test(message,cb)
     catch(error)
     {
         console.log(error);
-        console.log(message,+" Testing Unsucessfull");
+        console.log(message+" Testing Unsucessfull");
     }
 }
 
-function expect(actual)
-{
-    return
-    {
-        EqualTo:function abc(expected)
+function expect(actual){
+    return{
+        toEqual:function (expected)
         {
-            if(actual!==expect)
+            if(actual!==expected)
             {
-                throw new Error(`${actual} and ${expected} are not equal`);
+                throw new Error(`${actual} is not equal to ${expected}`);
+            }
+        },
+        toType:function (expected)
+        {
+            if(typeof actual!==expected)
+            {
+                throw new Error(`${actual} is not a ${expected}`);
             }
         }
     }
 }
+
+function add(num1,num2)
+{
+    return num1+num2;
+}
+
+function multiply(num1,num2)
+{
+    return num1*num2;
+}
+
+function testAdd()
+{
+    let result=add(4,6);
+    expect(result).toEqual(10);
+}
+
+
+function testMul()
+{
+    let result=multiply(4,6);
+    expect(result).toEqual(11);
+}
+
+test("addition of 4 and 6",testAdd);
+test("Multiplication of 4 and 6",testMul);
